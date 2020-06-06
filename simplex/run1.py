60f434958155f25e316517f0357cb9c03ef7b82a
huy27041998 <quanghuyle9@gmail.com>
2020-06-06 19:52:20 +0700

    update

--- simplex/run1.py
+++ simplex/run1.py
@@ -7,14 +7,14 @@ ap.add_argument("-f", "--file", required=True, help = 'Path to image')
 args = vars(ap.parse_args())
 # np.set_printoptions(suppress=True, formatter={'all':lambda x: str(Fraction(x).limit_denominator())})
 
-s = simplex_integer(args['file'], _print = False)
+s = simplex_integer(args['file'], _print = True)
 print('branch and bound')
 if (s.solve_branch_and_bound()):
     s.printSolution()
 else:
     print('Solution not found')
-print('gomory cut')
-if (s.solve_gomory()):
-    s.printSolution()
-else:
-    print('Solution not found')
+# print('gomory cut')
+# if (s.solve_gomory()):
+#     s.printSolution()
+# else:
+#     print('Solution not found')
